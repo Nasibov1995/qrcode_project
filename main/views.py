75e393965a73ba77ef921f60631ad991f2f1ea48
Nasibov1995 <nasib_az@mail.ru>
2024-05-01 02:08:13 +0400

    second

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 def scan_qr_code(request):
     if request.method == 'POST':
         scanned_url = request.POST.get('scanned_url')
-        # Process the scanned QR code data here
+        # Process the scanned QR code data here 
         # For demonstration purposes, let's just print the scanned data
         print("Scanned QR code data:", scanned_url)
         # You can add further processing logic here, such as saving to a database or performing some action
