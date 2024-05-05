87f7127cb80639f76b7a80d3e9e4afc7d58930c9
Nasibov1995 <nasib_az@mail.ru>
2024-05-05 22:35:18 +0400

    tenth commit convert to pdf done

--- main/models.py
+++ main/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 class Item(models.Model):
-    image_file = models.ImageField(upload_to='images',blank=True)
+    image_file = models.FileField(upload_to='qr_code_files',blank=True)
     image_url = RichTextField()
     
--- main/signals.py
+++ main/signals.py
@@ -3,7 +3,11 @@ from django.db.models.signals import post_save
 from django.core.files.base import ContentFile
 from . models import Item
 import base64
-
+from PIL import Image
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from django.http import HttpResponse
+from io import BytesIO
 
 @receiver(post_save, sender=Item)
 def save_image_from_base64(sender, instance, created, **kwargs):
@@ -13,11 +17,29 @@ def save_image_from_base64(sender, instance, created, **kwargs):
         image_data = base64.b64decode(base64_string)
 
         # Save the image to the image_file field
-        filename = "image.jpg"  # Or any other desired filename
-        instance.image_file.save(filename, ContentFile(image_data), save=False)
-        instance.save()
+        # filename = "image.jpg"  # Or any other desired filename
+        # instance.image_file.save(filename, ContentFile(image_data), save=False)
+        # instance.save()
         
-        return "Signals Worked"
+        # Open the image using PIL
+        img = Image.open(BytesIO(image_data))
+
+        # Create a PDF file
+        filename_pdf = "image.pdf"  # Or any other desired filename for PDF
+        pdf_file_path = instance.image_file.storage.path(filename_pdf)
+       
+
+        # Open the saved PDF file
+        with open(pdf_file_path, 'rb') as f:
+            # Save the PDF file to the image_file field
+            instance.image_file.save(filename_pdf, ContentFile(f.read()), save=False)
+            instance.save()
+
+        
+
+     
+        
+    return "Signals Worked"
 
 
 
